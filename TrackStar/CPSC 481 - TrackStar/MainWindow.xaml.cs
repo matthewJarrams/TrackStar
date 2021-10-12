8da6edd397b60b4b17bc4dd76abd66cd552ac804
matthewJarrams <matthew.jarrams@gmail.com>
2021-10-11 18:24:57 -0600

    Remade project with correct names

---
+++ TrackStar/CPSC 481 - TrackStar/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC_481___TrackStar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            completionBar.Maximum = 30;
+            completionBar.Value = Window1.completedWorkouts;
+        }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        Window1 objWindow1 = new Window1();
+        this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
+}
