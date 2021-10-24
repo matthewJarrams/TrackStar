31ea2abbd947569b4878b439024c239144abb79b
0wenH <owen.hunter@ucalgary.ca>
2021-10-24 15:23:01 -0600

    Meal page

--- TrackStar/CPSC 481 - TrackStar/Meals.xaml.cs
+++ TrackStar/CPSC 481 - TrackStar/Meals.xaml.cs
@@ -19,41 +19,76 @@ namespace CPSC_481___TrackStar
     /// </summary>
     public partial class Meals : Window
     {
-        public Meals()
-        {
-            InitializeComponent();
+       
+            public Meals()
+            {
+                InitializeComponent();
+                lvDataBinding.ItemsSource = mps;
         }
 
-        private void Home_Button_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
-            mainWindow.Visibility = Visibility.Visible;
-            this.Visibility = Visibility.Hidden;
-        }
+            public MealPlans[] mps { get; } =
+            new MealPlans[]
 
-        private void Cate_Button_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
-            mainWindow.catWindow.Show();
-            this.Visibility = Visibility.Hidden;
 
-        }
+            {
+            ("Keto", "","",
+            "This is a keto diet great for feeling like you're losing weight." ),
+            ("Weight gain", "","",
+            "This is the best way to get SWOLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE" ),
+            ("veggie", "","",
+            "Good for you, you are saving the animals but killing trees. " ),
+            ("Fast FOOD", "","",
+            "Mcdonalds is the best fastfood but wendy's has the best burgers. Dairy queen burgers are also very underrated and better than their ice cream" ),
+            ("Meat only", "","",
+            "Sorry to the animals it's not personal" ),
+            ("NUts only", "","",
+            "Great for protein and fats underrated food group." )
+            };
 
-        private void Info_Button_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
-            mainWindow.infoWindow.Show();
-            this.Visibility = Visibility.Hidden;
+            public class MealPlans
+            {
+                public string Description { get; set; }
+                public string Name { get; set; }
+                public string Title { get; set; }
+                public string Length { get; set; }
 
-        }
+                public static implicit operator MealPlans((string Name, string Title, string Length, string Description) info)
+                {
+                    return new MealPlans { Name = info.Name, Title = info.Title, Length = info.Length, Description = info.Description };
+                }
+            }
 
-        private void Goals_Button_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
-            mainWindow.goalsWindow.Show();
-            this.Visibility = Visibility.Hidden;
+            private void Home_Button_Click(object sender, RoutedEventArgs e)
+            {
+                MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
+                mainWindow.Visibility = Visibility.Visible;
+                this.Visibility = Visibility.Hidden;
+            }
 
+            private void Cate_Button_Click(object sender, RoutedEventArgs e)
+            {
+                MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
+                mainWindow.catWindow.Show();
+                this.Visibility = Visibility.Hidden;
+
+            }
+
+            private void Info_Button_Click(object sender, RoutedEventArgs e)
+            {
+                MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
+                mainWindow.infoWindow.Show();
+                this.Visibility = Visibility.Hidden;
+
+            }
+
+            private void Goals_Button_Click(object sender, RoutedEventArgs e)
+            {
+                MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
+                mainWindow.goalsWindow.Show();
+                this.Visibility = Visibility.Hidden;
+
+            }
         }
     }
-}
+
 
