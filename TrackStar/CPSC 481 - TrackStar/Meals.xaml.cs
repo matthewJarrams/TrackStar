8b940285c0aa55754541f706686788bbe28daa44
matthewJarrams <matthew.jarrams@gmail.com>
2021-10-30 15:14:12 -0600

    Added some extra stuff to meal screen

--- TrackStar/CPSC 481 - TrackStar/Meals.xaml.cs
+++ TrackStar/CPSC 481 - TrackStar/Meals.xaml.cs
@@ -55,17 +55,17 @@ namespace CPSC_481___TrackStar
         ("Ketogenic Diet", 
             "-Reduce Carb Intake \n -Burn Fat \n -Lose Weight",
             "BREAKFAST \n -Chocolate Keto Protein Shake \n -Avacado Toast \n  -Keto Cereal \n LUNCH \n -Keto Broccoli Salad \n -Keto Mac & Cheese \n -Cobb Egg Salad \n DINNER \n -Broiled Salmon \n -Cheesy Bacon Ranch Chicken \n -Garlic Rosemary Pork Chops \n",
-        "The ketogenic diet is a very low carb, high fat diet. It involves drastically reducing carbohydrate intake and replacing it with fat. This reduction in carbs puts your body into a metabolic state called ketosis. When this happens, your body becomes incredibly efficient at burning fat for energy." ),
+        "The ketogenic diet is a very low carb, high fat diet. It involves drastically reducing carbohydrate intake and replacing it with fat. This reduction in carbs puts your body into a metabolic state called ketosis. When this happens, your body becomes incredibly efficient at burning fat for energy.", "Calorie Intake: \t 2000 \nCarbs: \t\t 50g \nFat: \t\t 100g \nProtien: \t\t 80g" ),
         ("Healthy Weight-Gain", "","",
-        "This meal plan will add excess calories, but will do so with largely healthy foods. The types of foods eaten aren’t that dissimilar to foods eaten when dieting, the main difference is the sheer amount." ),
+        "This meal plan will add excess calories, but will do so with largely healthy foods. The types of foods eaten aren’t that dissimilar to foods eaten when dieting, the main difference is the sheer amount.","" ),
         ("Vegan Diet", "","",
-        "Good for you, you are saving the animals but killing trees. " ),
+        "Good for you, you are saving the animals but killing trees. ","" ),
         ("Fast FOOD", "","",
-        "Mcdonalds is the best fastfood but wendy's has the best burgers. Dairy queen burgers are also very underrated and better than their ice cream" ),
+        "Mcdonalds is the best fastfood but wendy's has the best burgers. Dairy queen burgers are also very underrated and better than their ice cream" ,""),
         ("Meat only", "","",
-        "Sorry to the animals it's not personal" ),
+        "Sorry to the animals it's not personal","" ),
         ("NUts only", "","",
-        "Great for protein and fats underrated food group." )
+        "Great for protein and fats underrated food group.","" )
         };
 
         public class MealPlans
@@ -75,9 +75,11 @@ namespace CPSC_481___TrackStar
             public string Goals { get; set; }
             public string Meals { get; set; }
 
-            public static implicit operator MealPlans((string Name, string Goals, string Meals, string Description) info)
+            public String Targets { get; set; }
+
+            public static implicit operator MealPlans((string Name, string Goals, string Meals, string Description, String Targets) info)
             {
-                return new MealPlans { Name = info.Name, Goals = info.Goals, Meals = info.Meals, Description = info.Description };
+                return new MealPlans { Name = info.Name, Goals = info.Goals, Meals = info.Meals, Description = info.Description, Targets = info.Targets};
             }
         }
 
